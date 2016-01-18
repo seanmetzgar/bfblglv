a5b0d76015ab3c83f53b080c1beda6a50de3c4e2
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:19:42 -0500

    trying row

--- entry-header.php
+++ entry-header.php
@@ -1,6 +1,6 @@
 <?php 
 	$has_post_thumbnail = ( has_post_thumbnail() );
-	$entry_header_class = $has_post_thumbnail ? "page-header has-image jumbotron" : "page-header";
+	$entry_header_class = $has_post_thumbnail ? "page-header has-image row" : "page-header row";
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
