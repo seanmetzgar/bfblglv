7b7aa8714658da0a3b350ab0c712fe66e5d6125f
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 06:50:53 -0500

    wrong meta field

--- bfbl-news-events-list.php
+++ bfbl-news-events-list.php
@@ -16,14 +16,12 @@
 		"post_type"			=> array( "events" ),
 		"nopaging"			=> false,
 		"posts_per_page"	=> 5,
-		"meta_key"			=> "date",
-		"order_by"			=> "meta_value",
+		"meta_key"			=> "event_date",
+		"order_by"			=> "meta_value_num",
 		"order"				=> "ASC"
 	);
 	$news_posts = get_posts($news_args);
 	$events_posts = get_posts($events_args);
-
-	echo ("<pre>" . print_r($events_posts, TRUE) . "</pre>");
 
 	$news_count = (is_array($news_posts) && count($news_posts)) ? count($news_posts) : 0;
 	$events_count = (is_array($events_posts) && count($events_posts)) ? count($events_posts) : 0; ?>
@@ -52,7 +50,7 @@
 				<header class="entry-header">
 					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					<p class="post-meta">
-						<span class="date"><?php the_field("date"); ?></span>
+						<span class="date"><?php the_field("event_date"); ?></span>
 					</p>
 				</header>
 				<div class="content">
