9fb5a6155f3061d107cbf1ba1af3631d924fd379
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:13:56 -0500

    adding bootstrap jumbotron class

---
+++ entry-header.php
@@ -0,0 +1,9 @@
+<?php 
+	$has_post_thumbnail = ( has_post_thumbnail() );
+	$entry_header_class = $has_post_thumbnail ? "entry-header has-image jumbotron" : "entry-header";
+?>
+					<header class="<?php echo $entry_header_class; ?>">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<?php if ($has_post_thumbnail) the_post_thumbnail("full"); ?>
+						<?php edit_post_link(); ?>
+					</header>
--- page.php
+++ page.php
@@ -2,12 +2,9 @@
 			<section class="main-content" role="main">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<header class="header">
-						<h1 class="entry-title"><?php the_title(); ?></h1> <?php edit_post_link(); ?>
-					</header>
+					<?php get_template_part("entry", "header"); ?>
 
 					<section class="entry-content">
-						<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 						<?php the_content(); ?>
 						<div class="entry-links"><?php wp_link_pages(); ?></div>
 					</section>
