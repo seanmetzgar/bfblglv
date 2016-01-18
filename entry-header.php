379e91dce0a2a7036d10d3bcce24c743c04cf432
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:23:30 -0500

    sdfasdf

--- entry-header.php
+++ entry-header.php
@@ -1,9 +1,9 @@
 <?php 
 	$has_post_thumbnail = ( has_post_thumbnail() );
-	$entry_header_class = $has_post_thumbnail ? "page-header has-image row" : "page-header row";
+	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
-						<?php if ($has_post_thumbnail) the_post_thumbnail("full"); ?>
+						<?php if ($has_post_thumbnail) the_post_thumbnail("full", array("class" => "img-responsive")); ?>
 						<?php edit_post_link(); ?>
 					</header>
