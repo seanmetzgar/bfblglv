1952d53bec745f2d5d4064d8ba0fb57b2e154f98
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 00:45:55 -0500

    sdfsdf

---
+++ front-page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+			<section class="main-content" role="main">
+			
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'entry' ); ?>
+				<?php comments_template(); ?>
+				<?php endwhile; endif; ?>
+
+			</section>
+<?php get_footer(); ?>
