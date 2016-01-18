56bf271551cefb8a4619a03ec0652bd05173a2d2
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 08:27:26 -0500

    fixing logic in news header

--- entry-news-events-header.php
+++ entry-news-events-header.php
@@ -1,16 +1,15 @@
 <?php 
-	$parent_id = $post->post_parent;
-	$header_image = get_field("ne_child_header_image", "option");
-	$header_image = is_int($header_image) ? wp_get_attachment_image($header_image, "full", array("class" => "img-responsive")) : false;
+	$parent_id = get_field("ne_child_back_page", "option");
+	$has_post_thumbnail = ( has_post_thumbnail($parent_id) );
 	$entry_header_class = $header_image ? "page-header has-image" : "page-header";
 	$back_button_text = get_field("ne_child_back_text", "option");
 	$back_button_text = strlen($back_button_text) > 0 ? $back_button_text : false;
-	$back_button_href = get_field("ne_child_back_page", "option");
-	$back_button_href = strlen($back_button_href) > 0 ? $back_button_page : false;
+	$back_button_href = get_permalink($parent_id);
+	$back_button_href = strlen($back_button_href) > 0 ? $back_button_href : false;
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
 						<p class="entry-title"><?php echo get_the_title($parent_id); ?></p>
-						<?php if ($header_image) echo $header_image; ?>
+						<?php if ($has_post_thumbnail) echo get_the_post_thumbnail($parent_id, "full", array("class" => "img-responsive")); ?>
 					</header>
 					<?php if ($back_button_href && $back_button_text): ?>
 					<a href="<?php echo $back_button_href; ?>" class="back-button"><p><?php echo $back_button_text; ?></p></a>
