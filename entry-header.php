8174d2529df053e134df0ee3845ece1b28600d3d
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:30:38 -0500

    ...

--- entry-header.php
+++ entry-header.php
@@ -2,8 +2,8 @@
 	$has_post_thumbnail = ( has_post_thumbnail() );
 	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
 ?>
-					<header class="<?php echo $entry_header_class; ?>">
+					<header class="<?php echo $entry_header_class; ?>"<?php echo $entry_header_style; ?>>
 						<h1 class="entry-title"><?php the_title(); ?></h1>
-						<?php if ($has_post_thumbnail) the_post_thumbnail("full", array("class" => "img-responsive")); ?>
 						<?php edit_post_link(); ?>
+						<?php if ($has_post_thumbnail) the_post_thumbnail("full", array("class" => "img-responsive")); ?>
 					</header>
