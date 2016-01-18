344a7a82c5b1c6737855c83617737de65d022595
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 04:47:31 -0500

    ...

--- entry-parent-header.php
+++ entry-parent-header.php
@@ -4,6 +4,6 @@
 	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
-						<p class="entry-title"><?php the_title(); ?></p>
+						<p class="entry-title"><?php the_title($parent_id); ?></p>
 						<?php if ($has_post_thumbnail) echo get_the_post_thumbnail($parent_id, "full", array("class" => "img-responsive")); ?>
 					</header>
