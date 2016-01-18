aa3c3d83948d233bc4cafb848fddc586d39ada64
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 07:20:32 -0500

    single-news / news-events header

---
+++ bfbl-news-events-header.php
@@ -0,0 +1,17 @@
+<?php 
+	$parent_id = $post->post_parent;
+	$header_image = get_field("ne_child_header_image");
+	$header_image = is_int($header_image) ? wp_get_attachment_image($header_image, "full", array("class" => "img-responsive")) : false;
+	$entry_header_class = $header_image ? "page-header has-image" : "page-header";
+	$back_button_text = get_field("ne_child_back_text", "option");
+	$back_button_text = strlen($back_button_text) > 0 ? $back_button_text : false;
+	$back_button_href = get_field("ne_child_back_href", "option");
+	$back_button_href = strlen($back_button_href) > 0 ? $back_button_page : false;
+?>
+					<header class="<?php echo $entry_header_class; ?>">
+						<p class="entry-title"><?php echo get_the_title($parent_id); ?></p>
+						<?php if ($header_image) echo $header_image; ?>
+					</header>
+					<?php if ($back_button_href && $back_button_text): ?>
+					<a href="<?php echo $back_button_href; ?>" class="back-button"><p><?php echo $back_button_text; ?></p></a>
+					<?php endif; ?>
