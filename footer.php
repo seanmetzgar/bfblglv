80a45fbfefa07295e4b126d2f326801f45272164
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:01:11 -0500

    adding about template file

--- footer.php
+++ footer.php
@@ -1,17 +1,6 @@
 
             <footer class="footer container" role="contentinfo">
-                <div class="copyright">
-                    <?php 
-                    	echo sprintf(
-                    		__("%1$s %2$s %3$s. All Rights Reserved.", "kudu"),
-                    			"&copy;",
-                    			date("Y"),
-                    			esc_html(get_bloginfo("name")));
-                    	echo " " . sprintf(
-                    		__("Theme By: %1$s.", "kudu" ),
-                    			"<a href=\"http://wearekudu.com/\">Kudu Creative</a>");
-                    ?>
-                </div>
+                
             </footer>
 
         </div><!-- END: .site-wrapper -->
--- front-page.php
+++ front-page.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php
+/**
+ * @package WordPress
+ * @subpackage Buy_Local_GLV
+ * @since Buy Local GLV 1.0.0
+ */
+get_header(); ?>
 			<section class="main-content" role="main">
 			
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
