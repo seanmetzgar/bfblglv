3c6ef2e207583851692501f70045811307c89bdc
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:45:33 -0500

    fixing header for pages

--- entry-header.php
+++ entry-header.php
@@ -2,7 +2,7 @@
 	$has_post_thumbnail = ( has_post_thumbnail() );
 	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
 ?>
-					<header class="<?php echo $entry_header_class; ?>"<?php echo $entry_header_style; ?>>
+					<header class="<?php echo $entry_header_class; ?>">
 						<h1 class="entry-title"><?php the_title(); ?></h1>
 						<?php edit_post_link(); ?>
 						<?php if ($has_post_thumbnail) the_post_thumbnail("full", array("class" => "img-responsive")); ?>
