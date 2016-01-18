53b217a0a8d273a5d6cbce5d0491f08d6f262443
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 09:35:17 -0500

    fixing header

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 			<section class="main-content" role="main">
-				<?php get_template_part("bfbl", "resources-header"); ?>
+				<?php get_template_part("entry", "resources-header"); ?>
 
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'entry' ); ?>
