0be944bb8799a66bdbdf680a4eec8b037f4b35cc
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 07:26:14 -0500

    sdfsdf

--- entry-news-events-header.php
+++ entry-news-events-header.php
@@ -1,11 +1,11 @@
 <?php 
 	$parent_id = $post->post_parent;
-	$header_image = get_field("ne_child_header_image");
+	$header_image = get_field("ne_child_header_image", "option");
 	$header_image = is_int($header_image) ? wp_get_attachment_image($header_image, "full", array("class" => "img-responsive")) : false;
 	$entry_header_class = $header_image ? "page-header has-image" : "page-header";
 	$back_button_text = get_field("ne_child_back_text", "option");
 	$back_button_text = strlen($back_button_text) > 0 ? $back_button_text : false;
-	$back_button_href = get_field("ne_child_back_href", "option");
+	$back_button_href = get_field("ne_child_back_page", "option");
 	$back_button_href = strlen($back_button_href) > 0 ? $back_button_page : false;
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
