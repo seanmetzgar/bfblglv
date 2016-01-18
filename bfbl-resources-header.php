cd2c5683b4b75d289f41d10b0ed6b921112f00fc
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 09:09:40 -0500

    some logic in the resource header

--- bfbl-resources-header.php
+++ bfbl-resources-header.php
@@ -1,16 +1,23 @@
 <?php 
 	$res_header_image = get_field("res_header_image", "option");
-	$has_post_thumbnail = ( has_post_thumbnail($parent_id) );
-	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
-	$back_button_text = get_field("ne_child_back_text", "option");
-	$back_button_text = strlen($back_button_text) > 0 ? $back_button_text : false;
-	$back_button_href = get_permalink($parent_id);
-	$back_button_href = strlen($back_button_href) > 0 ? $back_button_href : false;
+	$res_header_image = is_int($res_header_image) ? wp_get_attachment_image($res_header_image, "full") : false;
+	$res_header_title = get_field("res_archive_title", "option");
+	$res_header_title = strlen($res_header_title) > 0 ? $res_header_title : "Resources";
+	$res_header_back = get_field("res_back_button_text", "option");
+	$res_header_back = strlen($res_header_back) > 0 ? $res_header_back : "Back to Resources";
+
+	$entry_header_class = $res_header_image ? "page-header has-image" : "page-header";
+	$back_button_text = $res_header_back;
+	$back_button_href = get_post_type_archive_link($post_type);
 ?>
 					<header class="<?php echo $entry_header_class; ?>">
+						<?php if (is_single()): ?>
 						<p class="entry-title"><?php echo get_the_title($parent_id); ?></p>
-						<?php if ($has_post_thumbnail) echo get_the_post_thumbnail($parent_id, "full", array("class" => "img-responsive")); ?>
+						<?php else: ?>
+						<h1 class="entry-title"><?php echo get_the_title($parent_id); ?></h1>
+						<?php endif; ?>
+						<?php if ($res_header_image) echo $res_header_image; ?>
 					</header>
-					<?php if ($back_button_href && $back_button_text): ?>
+					<?php if ($back_button_href && $back_button_text && is_single()): ?>
 					<a href="<?php echo $back_button_href; ?>" class="back-button"><p><?php echo $back_button_text; ?></p></a>
 					<?php endif; ?>
