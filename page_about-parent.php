679867af28bfec39836349aba17cbfbf8f7a4d17
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-18 01:56:21 -0500

    mission statement block

--- page_about-parent.php
+++ page_about-parent.php
@@ -8,9 +8,20 @@
  */
 get_header(); ?>
 			<section class="main-content" role="main">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+					$show_mission = get_field("show_mission");
+					$show_mission = is_bool($show_mission) ? $show_mission : false; ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php get_template_part("entry", "header"); ?>
+
+					<?php if ($show_mission): ?>
+					<section class="mission-statement">
+						<div class="content">
+							<h2><?php the_field("mission_title"); ?></h2>
+							<?php the_field("mission_text"); ?>
+						</div>
+					</section>
+					<?php endif; ?>
 
 					<section class="entry-content">
 						<?php the_content(); ?>
