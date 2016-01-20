2ca7a9008c0aca35f08093e80735feb53404f4f5
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-20 12:59:12 -0500

    adding animated header to front-page.php

--- front-page.php
+++ front-page.php
@@ -5,41 +5,47 @@
  * @since Buy Local GLV 1.0.0
  */
 get_header(); ?>
-			<link rel="stylesheet" type="text/css" href=""
+			<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/animated-header.css">
 			<header class="animated-header" role="complementary">
 				<nav class="inner">
                     <ul class="animated-blocks">
-                        <li style="background-image: url(../images/header/about_photo.jpg);"><a href="#">
-                            <div class="content">
-                                <h3>About</h3>
-                                <div class="subtext"><p>This is an excerpt of what you'll find in About.</p></div>
-                            </div>
-                        </a></li>
-                        <li style="background-image: url(../images/header/wbl_photo.jpg);"><a href="#">
-                            <div class="content">
-                                <h3>Why Buy Local</h3>
-                                <div class="subtext"><p>This is an excerpt of what you'll find in WBL.</p></div>
-                            </div>
-                        </a></li>
-                        <li style="background-image: url(../images/header/ne_photo.jpg);"><a href="#">
-                            <div class="content">
-                                <h3>News &amp; Events</h3>
-                                <div class="subtext"><p>This is an excerpt of what you'll find in N&amp;E.</p></div>
-                            </div>
-                        </a></li>
-                        <li style="background-image: url(../images/header/res_photo.jpg);"><a href="#">
-                            <div class="content">
-                                <h3>Resources</h3>
-                                <div class="subtext"><p>This is an excerpt of what you'll find in Resources.</p></div>
-                            </div>
-                        </a></li>
-                        <li class="map-link"><a href="#" title="Find Local Food - View the map" class="map-link">
+                	<?php foreach ($block_counter = 1; $block_counter <= 5; $block_counter++): 
+                		$block_prefix = "block_$block_counter";
+                		$block_cta = get_field("{$block_prefix}_cta");
+                		$block_image = get_relative_file_path(get_field("{$block_prefix}_image"));
+                		$block_href = get_field("{$block_prefix}_href");
+                		$block_link_title = esc_attr($block_cta);
+                		if ($block_counter == 5):
+                			$block_title_small = get_field("{$block_prefix}_title_small");
+                			$block_title_small = strlen($block_title_small) > 0 ? $block_title_small : false;
+                			$block_title = get_field("{$block_prefix}_title");
+                			$block_badge = get_field("{$block_prefix}_badge");
+                			$block_badge = strlen($block_badge) > 0 ? get_relative_file_path($block_badge) : false;
+                    	?>
+                    	<li class="map-link" style="background-image: url('<?php echo $block_image; ?>');"><a href="<?php echo $block_href; ?>">
                             <div class="map-content">
-                                <div class="badge"><img src="/images/header/badge.png"></div>
-                                <h3>Find<br>Local<br>Food</h3>
-                                <p class="subtext">View the map</p>
+                            	<div class="inner">
+	                            	<?php if ($block_badge) echo "<div class=\"badge\"><img src=\"$block_badge\"></div>"; ?>	                                
+	                                <h3>
+	                                	<?php if ($block_title_small) echo "<small>$block_title_small</small>"; ?>
+	                                	<?php echo $block_title; ?>
+	                                </h3>
+	                                <p class="subtext"><?php echo $block_cta; ?></p>
+	                            </div>
                             </div>
                         </a></li>
+                    	<?php 
+                		else:
+                			$block_description = get_field("{$block_prefix}_description");
+                    	?>
+                        <li style="background-image: url('<?php echo $block_image; ?>');"><a href="<?php echo $block_href; ?>">
+                            <div class="content">
+                                <h3><?php echo $block_cta; ?></h3>
+                                <div class="subtext"><p><?php echo $block_description; ?></p></div>
+                            </div>
+                        </a></li>
+                    <?php endif;
+                	endforeach; ?>
                     </ul>
                 </nav>
 			</header>
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,21 @@
-jQuery(function () {
-      
+var $animatedHeader = null;
+
+//jQuery time
+$(function () {
+    "use strict";
+    $animatedHeader = $(".animated-blocks");
+    $animatedHeader.find("li").hover(function (e) {
+        var $this = $(this);
+        console.log(e);
+        if (e.type === "mouseenter") {
+            if (!$this.hasClass("map-link")) {
+                $animatedHeader.addClass("has-active").find("li").removeClass("active");
+                $this.addClass("active");
+            }
+        }
+        if (e.type === "mouseleave") {
+            $animatedHeader.removeClass("has-active").find("li").removeClass("active");
+        }
+    });
+
 });
