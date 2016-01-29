884928d099d884587ff72e78e78b6a84164dcf37
Sean Metzgar <sean.metzgar@gmail.com>
2016-01-29 10:20:30 -0500

    Updates from Webfoot 1.29.2016 - AM

--- entry-parent-header.php
+++ entry-parent-header.php
@@ -2,15 +2,29 @@
 	$parent_id = $post->post_parent;
 	$has_post_thumbnail = ( has_post_thumbnail($parent_id) );
 	$entry_header_class = $has_post_thumbnail ? "page-header has-image" : "page-header";
+
+	// newnewnew
+	$entry_header_css = '';
+	if($has_post_thumbnail) {
+		$thumbnail_id = get_post_thumbnail_id($parent_id);
+	//	$thumbnail_attrs = wp_get_attachment_metadata($thumbnail_id); // if needed
+		$thumbnail_src = wp_get_attachment_image_src($thumbnail_id, 'full');
+		$entry_header_css = "style='background-image: url({$thumbnail_src[0]});'";
+	} // end the is-there-a-post-thumbnail test
+
+	$bfblPageTitle = get_the_title($parent_id);
+	$bfblPageTitle = str_replace(' ', '<br />', $bfblPageTitle); // force line breaks between each word
+	
 	$back_button_text = get_field("child_back_text", $parent_id);
 	$back_button_text = strlen($back_button_text) > 0 ? $back_button_text : false;
 	$back_button_href = get_permalink($parent_id);
 	$back_button_href = strlen($back_button_href) > 0 ? $back_button_href : false;
 ?>
-					<header class="<?php echo $entry_header_class; ?>">
-						<p class="entry-title"><?php echo get_the_title($parent_id); ?></p>
+					<header class="<?php echo $entry_header_class; ?>" <?php echo $entry_header_css?>>
+						<p class="entry-title"><?php echo $bfblPageTitle; ?></p>
 						<?php if ($has_post_thumbnail) echo get_the_post_thumbnail($parent_id, "full", array("class" => "img-responsive")); ?>
 					</header>
+					
 					<?php if ($back_button_href && $back_button_text): ?>
-					<a href="<?php echo $back_button_href; ?>" class="back-button"><p><?php echo $back_button_text; ?></p></a>
+					<a href="<?php echo $back_button_href; ?>" class="back-button parentArrow"><?php echo $back_button_text; ?></a>
 					<?php endif; ?>
--- page_wbl-sub.php
+++ page_wbl-sub.php
@@ -9,21 +9,42 @@
 get_header(); ?>
 			<section class="main-content" role="main">
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+					
 					$has_video = get_field("has_video");
 					$has_video = is_bool($has_video) ? $has_video : false;
 					$video = $has_video ? get_field("video") : false;
+					
+					$page_icon = get_field('page_icon'); // if the page does not have this field, get_field() will return an empty string
+					
+					$contentClasses = array();
+					$contentClasses[] = 'entry-content';
+					if($has_video) {
+						$contentClasses[] = 'has-video';
+					}
+					if($page_icon && $page_icon != 'none') {
+						$contentClasses[] = 'has-icon';
+						$contentClasses[] = "icon-$page_icon";
+					}
+					
+					$contentClass = implode(' ', $contentClasses);
+					
+					
 				?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php get_template_part("entry", "parent-header"); ?>
 
-					<section class="entry-content<?php if ($has_video) echo " has-video"; ?>">
-						<h1><?php the_title(); ?></h1>
-						<?php edit_post_link(); ?>
+					<section class="<?php echo $contentClass; ?>">
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<?php // edit_post_link(); ?>
+						
 						<?php if ($has_video): ?>
 						<div class="video">
-							<?php echo $video; ?>
-						</div>
+							<div class="videoWrap">
+								<?php echo $video; ?>
+							</div><!-- end div.videoWrap -->
+						</div><!-- end div.video -->
 						<?php endif; ?>
+						
 						<?php the_content(); ?>
 					</section>
 
