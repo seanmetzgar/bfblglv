fae13dc62fddf3a9c58461ce43a06c82fcca873f
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-03 14:20:19 -0500

    Webfoot - 2.3.2016 - PM

--- 404.php
+++ 404.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
 			<section class="main-content" role="main">
 				<article id="post-0" class="post not-found">
-					<header class="header">
-						<h1 class="entry-title"><?php _e( 'Not Found', 'kudu' ); ?></h1>
-					</header>
-					<section class="entry-content">
-						<p><?php _e( 'Nothing found for the requested page. Try a search instead?', 'kudu' ); ?></p>
-						<?php get_search_form(); ?>
-					</section>
+					<h1>Holy cow!</h1>
+					<h2>You broke the Internet</h2>
+					
+					<p>Well ... techically speaking, you just went to a page that&rsquo;s out to pasture.</p>
+					<p class="not-found-btn">
+						<a class='bfblButtonLink btnBlue' href='<?php echo home_url('/local-food-map'); ?>'>
+							Find Local Food
+						</a>
+					</p>
+
+
 				</article>
 			</section>
-
-
 <?php get_footer(); ?>
