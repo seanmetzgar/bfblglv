bfc9517d7589173717d153e245d600982b71c7c1
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-04 09:22:27 -0500

    working through products arrays

--- author.php
+++ author.php
@@ -17,7 +17,36 @@ get_header(); ?>
 
 					$partner_bio = get_field("partner_description", $acf_partner_id);
 					$partner_bio = strlen($partner_bio) > 0 ? $partner_bio : false;
-					
+
+					$partner_category = get_user_role($current_partner_ID);
+
+					if ($partner_category === "farm") {
+						$products = array();
+						$products["greens"] = get_field("products_greens", $acf_partner_id);
+						$products["roots"] = get_field("products_roots", $acf_partner_id);
+						$products["seasonal"] = get_field("products_seasonal", $acf_partner_id);
+						$products["melons"] = get_field("products_melons", $acf_partner_id);
+						$products["herbs"] = get_field("products_herbs", $acf_partner_id);
+						$products["berries"] = get_field("products_berries", $acf_partner_id);
+						$products["small_fruits"] = get_field("products_small_fruits", $acf_partner_id);
+						$products["grains"] = get_field("products_grains", $acf_partner_id);
+						$products["value_added"] = get_field("products_value_added", $acf_partner_id);
+						$products["flowers"] = get_field("products_flowers", $acf_partner_id);
+						$products["plants"] = get_field("products_plants", $acf_partner_id);
+						$products["ornamentals"] = get_field("products_ornamentals", $acf_partner_id);
+						$products["syrups"] = get_field("products_syrups", $acf_partner_id);
+						$products["dairy"] = get_field("products_dairy", $acf_partner_id);
+						$products["meat"] = get_field("products_meat", $acf_partner_id);
+						$products["poultry"] = get_field("products_poultry", $acf_partner_id);
+						$products["agritourism"] = get_field("products_agritourism", $acf_partner_id);
+						$products["fibers"] = get_field("products_fibers", $acf_partner_id);
+						$products["artisinal"] = get_field("products_artisinal", $acf_partner_id);
+						$products["liquids"] = get_field("products_liquids", $acf_partner_id);
+						$products["educational"] = get_field("products_educational", $acf_partner_id);
+						$products["baked"] = get_field("products_baked", $acf_partner_id);
+						$products["seeds"] = get_field("products_seeds", $acf_partner_id);
+						$products["misc"] = get_field("products_misc", $acf_partner_id);
+					}					
 					
 				?>
 				<article id="partner-<?php the_ID(); ?>" class="partner-profile">
@@ -27,7 +56,7 @@ get_header(); ?>
 						<div class="marker" data-lat="<?php echo $location['partner_map']; ?>" data-lng="<?php echo $partner_map['lng']; ?>"></div>
 					</div></div>
 					<?php endif; ?>
-					
+
 					<section class="entry-content">
 						
 						<h1 class="entry-title"><?php echo $partner_name; ?></h1>
@@ -36,10 +65,13 @@ get_header(); ?>
 							<?php echo $partner_bio; ?>
 						</div>
 						<?php endif; ?>
-						
-						
-						
-						Content area
+						<?php
+						foreach($products as $productCategory=>$productCategoryProducts) {
+							echo ($productCategory);
+							echo ": ";
+							print_r($productCategoryProducts);
+							echo "<br><br>";
+						} ?>
 					</section>
 
 				</article>
