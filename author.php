2246efcb4559558653ea4f23bf0bd29e4a3e1b69
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-04 08:50:56 -0500

    outputting map

--- author.php
+++ author.php
@@ -11,6 +11,7 @@ get_header(); ?>
 					$current_partner_ID = $current_partner->ID;
 					$acf_partner_id = "user_{$current_partner_ID}";
 
+					$partner_map = get_field("partner_map", $acf_partner_id);
 					$partner_name = get_field("partner_name", $acf_partner_id);
 					$partner_name = strlen($partner_name) > 0 ? $partner_name : $current_partner->display_name;
 
@@ -21,9 +22,12 @@ get_header(); ?>
 				?>
 				<article id="partner-<?php the_ID(); ?>" class="partner-profile">
 					<?php //get_template_part("entry", "parent-header"); ?>
-					<header class="map-header">
-
-					</header>
+					<?php if( !empty($partner_map) ): ?>
+					<div class="acf-map-wrapper"><div class="acf-map">
+						<div class="marker" data-lat="<?php echo $location['partner_map']; ?>" data-lng="<?php echo $partner_map['lng']; ?>"></div>
+					</div></div>
+					<?php endif; ?>
+					
 					<section class="entry-content">
 						
 						<h1 class="entry-title"><?php echo $partner_name; ?></h1>
