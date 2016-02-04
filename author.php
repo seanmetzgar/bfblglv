32b9f5787d11dcb51b24bf09b0cd46dff837fdab
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-04 08:19:18 -0500

    ...

--- author.php
+++ author.php
@@ -10,7 +10,27 @@ get_header(); ?>
 					$current_partner = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 
 					print_r($current_partner);
-
+					
+					$contentClasses = array();
+					$contentClasses[] = 'entry-content';
+					$contentClasses[] = 'partner-content'
+					
+					$contentClass = implode(' ', $contentClasses);
+					
 				?>
+				<article id="partner-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php //get_template_part("entry", "parent-header"); ?>
+
+					<section class="<?php echo $contentClass; ?>">
+						
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+						<?php // edit_post_link(); ?>
+						
+						
+						
+						Content area
+					</section>
+
+				</article>
 			</section>
 <?php get_footer(); ?>
