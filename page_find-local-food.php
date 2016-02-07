4bb90b825a9ff474213f7c66804b50597e191b12
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-06 21:54:29 -0500

    form serialization

--- page_find-local-food.php
+++ page_find-local-food.php
@@ -53,8 +53,8 @@ get_header();
 	
 							<div class="form-inline page-block map-checkboxes">
 								<label><input type="checkbox" name="location_type[]" value="farm"><span>Farms</span></label>
-								<label><input type="checkbox" name="location_type[]" value="farm-csa"><span>CSAs</span></label>
-								<label><input type="checkbox" name="location_type[]" value="farm-farm_share"><span>Farm Shares</span></label>
+								<label><input type="checkbox" name="is_csa" value="1"><span>CSAs</span></label>
+								<label><input type="checkbox" name="is_farm_share" value="1"><span>Farm Shares</span></label>
 								<label><input type="checkbox" name="location_type[]" value="restaurant"><span>Restaurants / Caterers</span></label>
 								<label><input type="checkbox" name="location_type[]" value="retail"><span>Stores / Retail</span></label>
 								<label><input type="checkbox" name="location_type[]" value="vineyard"><span>Vineyards</span></label>
