3dd7b214ecc4e2cfe859ff3805f916be2e33d808
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-14 04:36:37 -0500

    resources archive update

--- archive-resources.php
+++ archive-resources.php
@@ -4,11 +4,41 @@
 				
 				<div class="resourcesContainer">
 					<div class="resourcesList">
-						<?php 
+						<!-- <?php 
 							if ( have_posts() ) : while ( have_posts() ) : the_post();
 								get_template_part( "entry", "resources-summary" );
 							endwhile; endif; 
+						?> -->
+						<?php $communityResourcesQuery = new WP_Query( array (
+							'post_type'              => array( 'resources' ),
+							'tag'                    => 'community',
+							'nopaging'               => false,
+							'posts_per_page'         => '-1',
+							'order'                  => 'ASC',
+							'orderby'                => 'title',
+						));
+						$publicationsResourcesQuery = new WP_Query( array (
+							'post_type'              => array( 'resources' ),
+							'tag'                    => 'reports',
+							'nopaging'               => false,
+							'posts_per_page'         => '-1',
+							'order'                  => 'ASC',
+							'orderby'                => 'title',
+						));
+						if ($communityResourcesQuery->have_posts()): 
+							echo "<h2>Find Locally Grown Foods</h2>";
+							while ($communityResourcesQuery->have_posts): $communityResourcesQuery->the_post();
+								get_template_part( "entry", "resources-summary" );
+							endwhile;
+						endif;
+						if ($publicationsResourcesQuery->have_posts()): 
+							echo "<h2>Publications</h2>";
+							while ($publicationsResourcesQuery->have_posts): $publicationsResourcesQuery->the_post();
+								get_template_part( "entry", "resources-summary" );
+							endwhile;
+						endif;
 						?>
+
 					</div><!-- end div.resourcesList -->
 				</div><!-- end div.resourcesContainer -->
 				
