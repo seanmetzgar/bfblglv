5df4a15cf2b83968c9e584b3f5a3ce119aa9dc53
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-14 05:01:16 -0500

    asdfasd

--- archive-resources.php
+++ archive-resources.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 			<section class="main-content" role="main">
 				<?php get_template_part("entry", "resources-header"); ?>
-				
+
 				<div class="resourcesContainer">
 					<div class="resourcesList">
-						<!-- <?php 
+						<!-- <?php
 							if ( have_posts() ) : while ( have_posts() ) : the_post();
 								get_template_part( "entry", "resources-summary" );
-							endwhile; endif; 
+							endwhile; endif;
 						?> -->
 						<?php $findResourcesQuery = new WP_Query( array (
 							'post_type'              => array( 'resources' ),
@@ -38,13 +38,13 @@
 							'order'                  => 'ASC',
 							'orderby'                => 'title'
 						));
-						if ($findResourcesQuery->have_posts()): 
+						if ($findResourcesQuery->have_posts()):
 							echo "<h2>Find Locally Grown Foods</h2>";
 							while ($findResourcesQuery->have_posts): $findResourcesQuery->the_post();
 								get_template_part( "entry", "resources-summary" );
 							endwhile;
 						endif;
-						if ($publicationsResourcesQuery->have_posts()): 
+						if ($publicationsResourcesQuery->have_posts()):
 							echo "<h2>Publications</h2>";
 							while ($publicationsResourcesQuery->have_posts): $publicationsResourcesQuery->the_post();
 								get_template_part( "entry", "resources-summary" );
@@ -54,7 +54,7 @@
 
 					</div><!-- end div.resourcesList -->
 				</div><!-- end div.resourcesContainer -->
-				
+
 				<?php get_template_part( 'nav', 'below' ); ?>
 			</section>
 
