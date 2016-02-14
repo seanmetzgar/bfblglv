a422f27b0570061f997c036058352c120507a0e5
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-13 23:02:25 -0500

    video on pages

--- bfbl-chips.php
+++ bfbl-chips.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 /**
  * @package WordPress
  * @subpackage Buy_Local_GLV
  * @since Buy Local GLV 1.0.0
  */
- 
+
  //	$page_object = get_queried_object();
 //	$page_id     = get_queried_object_id();
 //	$page_template = get_post_meta($page_id, "_wp_page_template");
@@ -13,19 +13,19 @@
 
 	$chipLoader = 'option';
 	$chipsOutput = "";
-	
+
 	if(have_rows('chips', $chipLoader)):
 		$chipsOutput .= '<section class="chips-block page-block tan-shadow">';
 			$chipsOutput .= '<nav role="complimentary">';
 				$chipsOutput .= '<ul>';
-		
+
 					while ( have_rows('chips', $chipLoader) ) : the_row();
-						
+
 						$chipTitle = get_sub_field('chip_title');
 						$chipDescr = get_sub_field('chip_description');
 						$chipImg = get_sub_field('chip_image');
 						$chipLink = get_sub_field('chip_page');
-						
+
 						$chipsOutput .= "<li class='chip'>";
 							$chipsOutput .= "<a href='$chipLink' class='chipLink'>";
 								$chipsOutput .= "<span class='chipImg' style='background-image: url(" . $chipImg['sizes']['medium'] . ")'>";
@@ -35,7 +35,7 @@
 								$chipsOutput .= "<span class='chipDescr'>$chipDescr</span>";
 							$chipsOutput .= "</a>";
 						$chipsOutput .= "</li><!-- end li.chip -->";
-						
+
 					endwhile;
 				$chipsOutput .= '</ul>';
 			$chipsOutput .= '</nav>';
--- page.php
+++ page.php
@@ -1,10 +1,21 @@
 <?php get_header(); ?>
 			<section class="main-content" role="main">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+					$has_video = get_field("has_video");
+					$has_video = is_bool($has_video) ? $has_video : false;
+					$video = $has_video ? get_field("video") : false; ?>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<?php get_template_part("entry", "header"); ?>
 
-					<section class="entry-content">
+					<section class="entry-content<?php if ($has_video) echo " has-video"; ?>">
+						<?php if ($has_video): ?>
+						<div class="video">
+							<div class="videoWrap">
+								<?php echo $video; ?>
+							</div><!-- end div.videoWrap -->
+						</div><!-- end div.video -->
+						<?php endif; ?>
+
 						<?php the_content(); ?>
 						<?php get_template_part("bfbl", "gallery"); ?>
 					</section>
