c9fda2f74a1fba8033c159745939040499187927
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-15 10:38:27 -0500

    ...

--- bfbl-sponsors.php
+++ bfbl-sponsors.php
@@ -21,6 +21,7 @@
 			if (has_post_thumbnail($sponsor_id)):
 				$sponsor_img_id = get_post_thumbnail_id($sponsor_id);
 				$sponsor_img_src = wp_get_attachment_image_src($sponsor_img_id, "medium");
+				$sponsor_img_src = $sponsor_img_src["url"];
 				$sponsor_name_attr = the_title_attribute(array("echo" => false));
 				$sponsor_link = get_field('url', $sponsor_id);
 ?>
