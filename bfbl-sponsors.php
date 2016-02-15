c4bf118435926b05067d82467d0373dfe5428936
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-15 10:31:16 -0500

    addressing issue with name attr

--- bfbl-sponsors.php
+++ bfbl-sponsors.php
@@ -21,7 +21,7 @@
 			if (has_post_thumbnail($sponsor_id)):
 				$sponsor_img_id = get_post_thumbnail_id($sponsor_id);
 				$sponsor_img_src = wp_get_attachment_image_src($sponsor_img_id, "medium");
-				$sponsor_name_attr = esc_attr(get_the_title());
+				$sponsor_name_attr = the_title_attribute(array("echo" => false));
 				$sponsor_link = get_field('url', $sponsor_id);
 ?>
 				<li class="col-md-3 col-xs-6 sponsor">
