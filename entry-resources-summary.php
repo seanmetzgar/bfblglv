920e88aeba1fad8cfce11f6088a5d9f6b72ace59
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-15 12:13:27 -0500

    adding post thumbnail to resource

--- entry-resources-summary.php
+++ entry-resources-summary.php
@@ -10,40 +10,16 @@ if (is_array($resourceTaxonomyTermsArray) && count($resourceTaxonomyTermsArray) 
 	$resourceTaxonomyTerms = implode(", ", $resourceTaxonomyTerms);
 }
 
-// UNCOMMENT TO RESTORE
-/*
-if(has_post_thumbnail()) {
-	$thumbnail_id = get_post_thumbnail_id();
-//	$thumbnail_attrs = wp_get_attachment_metadata($thumbnail_id); // if needed
-	$thumbnail_src = wp_get_attachment_image_src($thumbnail_id, 'full');
-	$resourceLinkCSS = "style='background-image: url({$thumbnail_src[0]});'";
-} // end the is-there-a-post-thumbnail test
-*/
-
-echo "<article class='resourceLink' $resourceLinkCSS >";
-	echo "<h2>";
+echo "<article class='resource-link' $resourceLinkCSS >";
+	echo "<h3>";
 		echo "<a href='";
 			the_permalink();
 		echo "'>";
 			echo "<span>";
 				the_title();
-				// if (is_string($resourceTaxonomyTerms) && strlen($resourceTaxonomyTerms) > 0) {
-				// echo "<span class='categories'>$resourceTaxonomyTerms</span>";
-				// }
 			echo "</span>";
 		echo "</a>";
-	echo "</h2>";
+	echo "</h3>";
+	if (has_post_thumbnail()) { the_post_thumbnail(); }
 echo "</article>";
-
-
-/*
-<article class="col-md-4"><a href="<?php the_permalink(); ?>">
-	<div class="image">
-		<?php if (has_post_thumbnail()) the_post_thumbnail("medium", array("class" => "img-responsive")); ?>
-	</div>
-	<div class="content">
-		<h2><?php the_title(); ?></h2>
-	</div>
-</a></article>
-*/
 ?>
