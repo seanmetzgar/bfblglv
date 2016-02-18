86cde44b7c4413642fabee8a29ea54752b7e2b53
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-18 14:33:43 -0500

    asdf

--- archive-resources.php
+++ archive-resources.php
@@ -34,7 +34,7 @@
 					<div class="resources-list row">
 						<div class="col-sm-6 resource-category-list">
 							<?php  if ($findResourcesQuery->have_posts()):
-								echo "<h2>Find Locally Grown Foods</h2>\n";
+								echo "<h2>Find Locally<br>Grown Foods</h2>\n";
 								echo "<div class=\"resourcesList\">\n";
 								while ($findResourcesQuery->have_posts()): $findResourcesQuery->the_post();
 									get_template_part( "entry", "resources-summary" );
@@ -45,7 +45,7 @@
 
 						<div class="col-sm-6 resource-category-list">
 							<?php if ($publicationsResourcesQuery->have_posts()):
-								echo "<h2>Publications</h2>";
+								echo "<h2>Our<br>Publications</h2>";
 								echo "<div class=\"resourcesList\">\n";
 								while ($publicationsResourcesQuery->have_posts()): $publicationsResourcesQuery->the_post();
 									get_template_part( "entry", "resources-summary" );
