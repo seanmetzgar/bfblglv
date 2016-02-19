431336df2285705bf7dfc787fae26a63df5823fe
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-19 03:11:26 -0500

    ...

--- inc/_inc_ajax.php
+++ inc/_inc_ajax.php
@@ -666,7 +666,7 @@ function xhrAddPartner() {
 		"user_registered" => $member_since
 	);
 	$user_id = wp_insert_user($new_user_args);
-	wp_new_user_notification($user_id);
+	wp_new_user_notification($user_id, null, "both");
 
 	$response = array();
 	if (is_int($user_id) && $user_id > 0) {
