69ecc3a82398e16be8144a0e2f90875de81cf29e
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-19 06:24:02 -0500

    sdfsdf

--- inc/_inc_ajax.php
+++ inc/_inc_ajax.php
@@ -90,9 +90,6 @@ function splitHours($hours) {
 				if (strlen($hoursString) > 0) {
 					$hoursRegex = "/(\d{1,2}:\d{2})(\w{2})-(\d{1,2}:\d{2})(\w{2})\s*/i";
 					preg_match_all($hoursRegex, $hoursString, $tempMatch);
-					echo "<pre>";
-					print_r($tempMatch);
-					echo "</pre>";
 					if (count($tempMatch) == 5) {
 						$tempStart = $tempMatch[1][0];
 						$tempStartMeridian = $tempMatch[2][0];
@@ -129,7 +126,10 @@ function splitHours($hours) {
 			}
 			$hoursReturn[] = $tempObj;
 		}
+
+		echo "<pre>";
 		print_r($hoursReturn);
+		echo "</pre>";
 	}
 	return $hoursReturn;
 }
