302999779900519e81888dd85ad54df8c6c1d038
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-22 02:00:52 -0500

    asdf

--- entry-meta.php
+++ entry-meta.php
@@ -1,3 +1,7 @@
 <section class="entry-meta">
+	<?php if ($post_type !== "events"): ?>
 	<span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
+	<?php else: ?>
+	<span class="entry-date"><?php the_field("event_date");?></span>
+	<?php endif; ?>
 </section>
