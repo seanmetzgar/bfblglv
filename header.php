17ea2b9cd892aa877fb96e9e7f9e572dae12cf89
Sean Metzgar <sean.metzgar@gmail.com>
2016-02-24 16:04:29 -0500

    pixel code manually on pages

--- header.php
+++ header.php
@@ -162,6 +162,21 @@
         <?php wp_head(); ?>
         <!-- end wp_head() -->
 
+        <!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '428194407374064');
+		fbq('track', "PageView");</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=428194407374064&ev=PageView&noscript=1"
+		/></noscript>
+		<?php echo '<!-- ' . basename( get_page_template() ) . ' -->'; ?>
+		<!-- End Facebook Pixel Code -->
     </head>
 
     <body <?php body_class(); ?>>
