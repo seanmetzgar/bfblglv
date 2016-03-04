c957d1e740b33930d12f8bdc9d812803774e39c8
Sean Metzgar <sean.metzgar@gmail.com>
2016-03-04 17:13:38 -0500

    trying disabled checkbox for wholesale when logged out

--- page_find-local-food.php
+++ page_find-local-food.php
@@ -148,7 +148,7 @@ get_header();
                                 $wholesaleChecked = (isset($_REQUEST["wholesale"]) && ($_REQUEST["wholesale"] == "true" || $_REQUEST["wholesale"] == "1")) ? true : false; ?>
 							<label class='greenHeader'><input type="checkbox" name="wholesale" value="1"<?php if ($wholesaleChecked) { echo " checked"; } ?>><span>Wholesale</span></label>
                             <?php else: ?>
-							<div class='greenHeader pseudoWholesale'><span>Wholesale (Must be logged in for access)</span></div>
+							<label class='greenHeader'><input disabled type="checkbox" name="wholesale" value="1"><span>Wholesale</span></label>
                             <?php endif; ?>
 						</section>
 					</form>
