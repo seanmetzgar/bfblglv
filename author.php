ccb443042a5e0cf1b27b3296bfcc504189330b56
Sean Metzgar <sean.metzgar@gmail.com>
2016-07-15 17:35:49 -0400

    debug

--- author.php
+++ author.php
@@ -890,7 +890,8 @@ get_header(); ?>
 									if (is_array($shares_product_type) && count($shares_product_type) > 0) {
 										$tempShareProductTypeArray = [];
 										foreach ($shares_product_type as $temp_type) {
-											$tempShareProductTypeArray[] = niceProductTypeName($temp_type);
+											echo "<p>$temp_type</p>";
+											//$tempShareProductTypeArray[] = niceProductTypeName($temp_type);
 										}
 										$shares_product_type = implode(", ", $tempShareProductTypeArray);
 									} elseif (is_string($shares_product_type) && strlen($shares_product_type) > 0) {
