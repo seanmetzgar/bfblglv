4fe178b986fdb6504f81b13e5fe4beb1bb95f1f0
Sean Metzgar <sean.metzgar@gmail.com>
2016-11-30 21:39:49 -0500

    aaaa

--- inc/_inc_ajax.php
+++ inc/_inc_ajax.php
@@ -1051,7 +1051,7 @@ function xhrGetPartners() {
 
 	$updatedSpecificProductsList = array_values(array_unique($updatedSpecificProductsList, SORT_REGULAR));
 	foreach ($updatedSpecificProductsList as $updatedSpecificProduct) {
-		$isActive = (is_array($specificProducts) ? in_array($updatedSpecificProduct, $specificProducts) : false;
+		$isActive = (is_array($specificProducts)) ? in_array($updatedSpecificProduct, $specificProducts) : false;
 		$updatedSpecificProduct = new SpecificProduct($updatedSpecificProduct, $isActive);
 	}
 
