c7e67dad394a73f148e6d4409a4bb54ac36dddac
Sean Metzgar <sean.metzgar@gmail.com>
2017-02-06 14:40:24 -0500

    Fixing order by date

--- inc/_inc_shortcodes.php
+++ inc/_inc_shortcodes.php
@@ -129,10 +129,23 @@ function blglv_modify_user_table( $column ) {
 }
 add_filter( 'manage_users_columns', 'blglv_modify_user_table' );
 function blglv_modify_user_sortable( $columns ) {
+	$columns['partner_name'] = 'partner_name';
     $columns['user_modified'] = 'user_modified';
     return $columns;
 }
 add_filter( 'manage_users_sortable_columns', 'blglv_modify_user_sortable' );
+
+function blglv_user_modified_sort( $vars ) {
+if ( isset( $vars['orderby'] ) && 'user_modified' == $vars['orderby'] ) {
+    $vars = array_merge( $vars, array(
+        'meta_key' => 'user_last_updated',
+        'orderby' => 'meta_value'
+    ) );
+}
+
+return $vars;
+}
+add_filter( 'request', 'blglv_user_modified_sort' );
 
 function blglv_modify_user_table_row( $val, $column_name, $user_id ) {
     switch ($column_name) {
