d37df0768ef864279e05c35f18318e935c03dc4f
Sean Metzgar <sean.metzgar@gmail.com>
2017-02-16 21:29:16 -0500

    Adding changes from Lynn for Agritourism/CSA pages

--- page-partner-type.php
+++ page-partner-type.php
@@ -85,18 +85,25 @@ get_header();
 					<?php if (!empty($partners)): ?>
 					<section class="page-block odd">
 						<ul class="partners-blocks row">
-							<?php foreach ($partners as $partner): 
+							<?php foreach ($partners as $partner):
 								$partner_ID 	= $partner->ID;
 								$acf_partner_id = "user_{$partner_ID}";
 								$partner_url 	= get_author_posts_url($partner_ID);
 								$partner_name 	= get_field("partner_name", $acf_partner_id);
-								$partner_name 	= strlen($partner_name) > 0 ? 
-												$partner_name : 
+								$partner_name 	= strlen($partner_name) > 0 ?
+												$partner_name :
 												$partner->display_name;
+								$partner_city = get_field("partner_city", $acf_partner_id);
+								$partner_city = strlen($partner_city) > 0 ? $partner_city : false;
+								$partner_state = get_field("partner_state", $acf_partner_id);
+								$partner_state = strlen($partner_state) > 0 ? $partner_state : false;
+								$partner_location = ($partner_city) ? $partner_city : "";
+								$partner_location = ($partner_city && $partner_state) ? ", $partner_state" : (($partner_state) ? $partner_state : "");
+
 								//Images
 									//Logo
 								$partner_logo 	= get_field("logo", $acf_partner_id);
-								$partner_logo  	= (is_array($partner_logo) && array_key_exists("url", $partner_logo)) ? 
+								$partner_logo  	= (is_array($partner_logo) && array_key_exists("url", $partner_logo)) ?
 												$partner_logo["url"] :
 												false;
 								$partner_fill = ($partner_logo) ? "contain" : "cover";
@@ -107,7 +114,7 @@ get_header();
 								} elseif (is_string($partner_owner_photo) && $partner_owner_photo) {
 									$partner_owner_photo = trim($partner_owner_photo);
 								} else { $partner_owner_photo = false; }
-									
+
 									//Business
 								$partner_business_photo = get_field("business_photo", $acf_partner_id);
 
@@ -124,15 +131,15 @@ get_header();
 							<li class="col-md-3 col-sm-4 col-xs-6"><a href="<?php echo $partner_url; ?>" title="<?php echo $partner_name; ?>">
 								<?php if ($partner_image): ?>
 								<figure class="image <?php echo $partner_fill; ?>" style="background-image: url('<?php echo $partner_image; ?>');">
-									<span><?php echo $partner_name; ?></span>
+									<span><?php echo $partner_name; ?><?php if ($partner_location) echo "<br><em>$partner_location</em>"; ?></span>
 								</figure>
 								<?php else: ?>
 								<div class="pseudo-image">
-									<span><?php echo $partner_name; ?></span>
+									<span><?php echo $partner_name; ?><?php if ($partner_location) echo "<br><em>$partner_location</em>"; ?></span>
 								</div>
 								<?php endif; ?>
 								<div class="overlay">
-									<span><?php echo $partner_name; ?></span>
+									<span><?php echo $partner_name; ?><?php if ($partner_location) echo "<br><em>$partner_location</em>"; ?></span>
 								</div>
 							</a></li>
 							<?php endforeach; ?>
