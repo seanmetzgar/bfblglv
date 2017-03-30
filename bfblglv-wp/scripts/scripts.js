42a963e022650b800416057fa3e3b3252026b007
Sean Metzgar <sean.metzgar@gmail.com>
2017-03-30 15:46:21 -0400

    Should be working now

--- bfblglv-wp/scripts/scripts.js
+++ bfblglv-wp/scripts/scripts.js
@@ -246,7 +246,6 @@ jQuery(document).ready(function ($) {
         var product = $this.data("product");
         var $partnerBlocks = $(".partner-blocks");
         e.preventDefault();
-        console.log(product);
         if (product === "all") {
             $partnerBlocks.find("li").show();
         } else {
