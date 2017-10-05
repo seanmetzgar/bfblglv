dd017f7fe6bd1934bd4b3b0691ee175953fa27e7
Sean Metzgar <sean.metzgar@gmail.com>
2017-10-05 15:03:52 -0400

    Adding post thumbnail to template

--- bfblglv-wp/entry.php
+++ bfblglv-wp/entry.php
@@ -8,4 +8,7 @@
 	<?php if (is_archive() || is_search()):?>
 		<p><a href="<?php the_permalink(); ?>" class="read-more">Read More</a></p>
 	<?php endif; ?>
+	<?php if (is_archive() & has_post_thumbnail()): ?>
+		<figure class="post-thumbnail"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail();?></a></figure>
+	<?php endif; ?>
 </article>
