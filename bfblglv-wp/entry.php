1462834e89c4029a524918904dfd116a277def7d
Sean Metzgar <sean.metzgar@gmail.com>
2017-10-05 15:07:52 -0400

    Adding more logic for post_class on entry blocks

--- bfblglv-wp/entry.php
+++ bfblglv-wp/entry.php
@@ -1,4 +1,13 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php
+	$post_class = [];
+	if ($post_type === "news" && is_archive()) {
+		$post_class[] = "page-block";
+		if (has_post_thumbnail()) {
+			$post_class[] = "image-right";
+		}
+	}
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class($post_class); ?>>
 	<header>
 		<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php // edit_post_link(); ?>
 		<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
