b211aca0f4679cda74b5753a80b86ef882da1838
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:45:09 -0400

    adding classes to post

--- snap/snap-wp/entry-faq.php
+++ snap/snap-wp/entry-faq.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('my-3 mb-4'); ?>>
     <header>
         <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><span><?php the_title(); ?></span></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
     </header>
