d647767453cc2f15d58ea9c293f37333af614993
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:31:10 -0400

    Adding Q before question

---
+++ snap/snap-wp/entry-faq.php
@@ -0,0 +1,7 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header>
+        <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><span><?php the_title(); ?></span></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+    </header>
+    <?php get_template_part( 'entry', 'content' ); ?>
+    <?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
+</article>
