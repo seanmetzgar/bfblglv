97e3610599e9b792b82eb2c3d3a41f064db77eec
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:24:19 -0400

    Moving edit link

--- snap/snap-wp/entry-footer.php
+++ snap/snap-wp/entry-footer.php
@@ -1,4 +1,5 @@
 <footer class="entry-footer">
+    <?php edit_post_link(); ?>
     <?php if ( comments_open() ) {
         echo '<span class="meta-sep">|</span> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'snap-wp' ) ) . '</a></span>';
     } ?>
