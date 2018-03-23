76ca0cf908bf10fc05260b89955629de00f46a55
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 10:59:26 -0400

    Fixing spacing issues

--- snap/snap-wp/archive.php
+++ snap/snap-wp/archive.php
@@ -1,17 +1,16 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php 
-if ( is_day() ) { printf( __( 'Daily Archives: %s', 'snap-wp' ), get_the_time( get_option( 'date_format' ) ) ); }
-elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'snap-wp' ), get_the_time( 'F Y' ) ); }
-elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'snap-wp' ), get_the_time( 'Y' ) ); }
-else { _e( 'Archives', 'snap-wp' ); }
-?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+    <section class="container" role="main">
+        <header class="header">
+            <h1 class="entry-title"><?php
+                if ( is_day() ) { printf( __( 'Daily Archives: %s', 'snap-wp' ), get_the_time( get_option( 'date_format' ) ) ); }
+                elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'snap-wp' ), get_the_time( 'F Y' ) ); }
+                elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'snap-wp' ), get_the_time( 'Y' ) ); }
+                else { _e( 'Archives', 'snap-wp' ); }
+                ?></h1>
+        </header>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'entry' ); ?>
+        <?php endwhile; endif; ?>
+        <?php get_template_part( 'nav', 'below' ); ?>
+    </section>
 <?php get_footer(); ?>
--- snap/snap-wp/tag.php
+++ snap/snap-wp/tag.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-<section id="content" role="main">
-<header class="header">
-<h1 class="entry-title"><?php _e( 'Tag Archives: ', 'snap-wp' ); ?><?php single_tag_title(); ?></h1>
-</header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</section>
-<?php get_sidebar(); ?>
+    <section id="content" role="main">
+        <header class="header">
+            <h1 class="entry-title"><?php _e( 'Tag Archives: ', 'snap-wp' ); ?><?php single_tag_title(); ?></h1>
+        </header>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'entry' ); ?>
+        <?php endwhile; endif; ?>
+        <?php get_template_part( 'nav', 'below' ); ?>
+    </section>
 <?php get_footer(); ?>
