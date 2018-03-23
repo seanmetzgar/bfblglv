d5720eb7a9e4579204d314f5d468a818df51ca8a
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:15:49 -0400

    Forgot to add files

---
+++ snap/snap-wp/archive-faq.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+    <section class="container" role="main">
+        <header class="header text-center text-secondary mt-4 mb-3">
+            <h1 class="entry-title my-0">Frequently Asked Questions</h1>
+        </header>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part( 'entry' ); ?>
+        <?php endwhile; endif; ?>
+        <?php get_template_part( 'nav', 'below' ); ?>
+    </section>
+<?php get_footer(); ?>
