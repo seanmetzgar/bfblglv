ec937de9fe6451330881d74a24503dd23c865079
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:26:45 -0400

    prepping FAQs for javascript

--- snap/snap-wp/archive-faq.php
+++ snap/snap-wp/archive-faq.php
@@ -4,7 +4,7 @@
             <h1 class="entry-title my-0">Frequently Asked Questions</h1>
         </header>
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'entry' ); ?>
+            <?php get_template_part( 'entry', $post_type ); ?>
         <?php endwhile; endif; ?>
         <?php get_template_part( 'nav', 'below' ); ?>
     </section>
