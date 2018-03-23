35c045687746ef1a897f8fbebc7f6635cb9405b2
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-23 11:00:52 -0400

    generic archive template fixes

--- snap/snap-wp/archive.php
+++ snap/snap-wp/archive.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <section class="container" role="main">
-        <header class="header">
-            <h1 class="entry-title"><?php
+        <header class="header text-center text-secondary mt-4 mb-3">
+            <h1 class="entry-title my-0"><?php
                 if ( is_day() ) { printf( __( 'Daily Archives: %s', 'snap-wp' ), get_the_time( get_option( 'date_format' ) ) ); }
                 elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'snap-wp' ), get_the_time( 'F Y' ) ); }
                 elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'snap-wp' ), get_the_time( 'Y' ) ); }
