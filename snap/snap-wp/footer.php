4a57cd9f41fd4f69f8ce9eb40957a2f254c7a3d2
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-29 12:52:03 -0400

    Fixing logo in footer

--- snap/snap-wp/footer.php
+++ snap/snap-wp/footer.php
@@ -5,7 +5,7 @@
         <div class="container justify-content-center">
             <div class="row">
                 <div class="col-md-2 col-12 footer-logo justify-content-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png" class="img-fluid">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png">
                 </div>
                 <nav class="col-md-6 col-12 footer-nav">
                     <h3>Navigate</h3>
