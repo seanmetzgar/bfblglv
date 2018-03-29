11d5d6fa5ac4033bbd9ffb45492de2d3e1dc2098
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-29 10:38:45 -0400

    Adding logo to footer

--- snap/snap-wp/footer.php
+++ snap/snap-wp/footer.php
@@ -3,7 +3,10 @@
 
     <footer class="site-footer container-fluid" role="contentinfo">
         <div class="row">
-            <nav class="col-12 footer-nav">
+            <div class="col-2 footer-logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png" class="img-fluid">
+            </div>
+            <nav class="col-8 footer-nav">
                 <ul>
                     <li><a href="/">Home</a></li>
                     <li><a href="#">Locations</a></li>
