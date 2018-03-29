71e04b0a41fcad19553c4b446916726f3046793b
Sean Metzgar <sean.metzgar@gmail.com>
2018-03-29 11:08:13 -0400

    Adding wordpress footer menu

--- snap/snap-wp/footer.php
+++ snap/snap-wp/footer.php
@@ -2,21 +2,20 @@
     </div>
 
     <footer class="site-footer" role="contentinfo">
-        <div class="container">
+        <div class="container justify-content-center">
             <div class="row">
                 <div class="col-md-2 col-12 footer-logo">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png" class="img-fluid">
                 </div>
                 <nav class="col-md-6 col-12 footer-nav">
                     <h3>Navigate</h3>
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="#">Locations</a></li>
-                        <li><a href="#">How it Works</a></li>
-                        <li><a href="#">FAQs</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Resources</a></li>
-                    </ul>
+                    <?php
+                        wp_nav_menu([
+                            'theme_location'  => 'main-menu',
+                            'container'       => false,
+                            'depth'           => 1
+                        ]);
+                    ?>
                 </nav>
                 <div class="col-md-4 col-12 footer-newsletter">
                     <h3>Connect with us</h3>
