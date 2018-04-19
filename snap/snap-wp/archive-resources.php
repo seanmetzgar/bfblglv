bbe13e9b1bbe67ca346868d22af9ad5c948fcf54
Sean Metzgar <sean.metzgar@gmail.com>
2018-04-19 15:59:24 -0400

    ...

--- snap/snap-wp/archive-resources.php
+++ snap/snap-wp/archive-resources.php
@@ -6,7 +6,8 @@
 
         <div class="row justify-content-center">
             <div class="col-md-5 col-sm-6">
-                <h2>Community Partners Resources</h2>
+                <h4>Community Partners Resources</h4>
+                <div class="list-group">
                 <?php
                     // WP_Query arguments
                     $args = array(
@@ -27,41 +28,44 @@
                     // The Loop
                     if ( $query->have_posts() ) {
                         while ( $query->have_posts() ) {
-                            $query->the_post();
-                            get_template_part( 'entry', $post_type);
-                        }
+                            $query->the_post(); ?>
+                            <a href="<?php the_permalink(); ?>" class="list-group-item list-group-item-action"><?php the_title(); ?></a>
+                        <?php }
                     }
                     wp_reset_postdata();
                 ?>
+                </div>
             </div>
             <div class="col-md-5 col-sm-6">
-                <h2>Vendor Resources</h2>
-                <?php
-                // WP_Query arguments
-                $args = array(
-                    'post_type'              => array( 'resources' ),
-                    'posts_per_page'         => '-1',
-                    'tax_query'              => array(
-                        array(
-                            'taxonomy'         => 'sponsor_type',
-                            'terms'            => 'vendors',
-                            'field'            => 'slug',
+                <h4>Vendor Resources</h4>
+                <div class="list-group">
+                    <?php
+                    // WP_Query arguments
+                    $args = array(
+                        'post_type'              => array( 'resources' ),
+                        'posts_per_page'         => '-1',
+                        'tax_query'              => array(
+                            array(
+                                'taxonomy'         => 'sponsor_type',
+                                'terms'            => 'vendors',
+                                'field'            => 'slug',
+                            ),
                         ),
-                    ),
-                );
+                    );
 
-                // The Query
-                $query = new WP_Query( $args );
+                    // The Query
+                    $query = new WP_Query( $args );
 
-                // The Loop
-                if ( $query->have_posts() ) {
-                    while ( $query->have_posts() ) {
-                        $query->the_post();
-                        get_template_part( 'entry', $post_type);
+                    // The Loop
+                    if ( $query->have_posts() ) {
+                        while ( $query->have_posts() ) {
+                            $query->the_post(); ?>
+                            <a href="<?php the_permalink(); ?>" class="list-group-item list-group-item-action"><?php the_title(); ?></a>
+                        <?php }
                     }
-                }
-                wp_reset_postdata();
-                ?>
+                    wp_reset_postdata();
+                    ?>
+                </div>
             </div>
         </div>
     </section>
