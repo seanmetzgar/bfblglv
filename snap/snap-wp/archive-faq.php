4e2592ad6d48b3eed5cc4353390ce3e9d2714928
Sean Metzgar <sean.metzgar@gmail.com>
2018-04-19 15:35:45 -0400

    spacing in accordion

--- snap/snap-wp/archive-faq.php
+++ snap/snap-wp/archive-faq.php
@@ -9,9 +9,9 @@
             <article class="card">
                 <div class="card-header" id="heading-<?php the_ID(); ?>">
                     <a href="#post-<?php the_ID(); ?>" class="collapsed accordion-link" data-toggle="collapse" data-target="#post-<?php the_ID(); ?>" aria-expanded="true" aria-controls="heading-<?php the_ID(); ?>">
-                        <h2 class="entry-title">
+                        <h4>
                             <?php the_title(); ?>
-                        </h2>
+                        </h4>
                     </a>
                 </div>
                 <div id="post-<?php the_ID(); ?>" class="collapse" aria-labelledby="#heading-<?php the_ID(); ?>" data-parent="#faq-accordion">
