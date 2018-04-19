08d84cedd5e0103ef86aee7d1ecad35a7f1478e5
Sean Metzgar <sean.metzgar@gmail.com>
2018-04-19 15:05:26 -0400

    Archive accordion for FAQs

--- snap/snap-wp/archive-faq.php
+++ snap/snap-wp/archive-faq.php
@@ -3,9 +3,26 @@
         <header class="header text-center text-secondary mt-4 mb-3">
             <h1 class="entry-title my-0">Frequently Asked Questions</h1>
         </header>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'entry', $post_type ); ?>
-        <?php endwhile; endif; ?>
+        <?php if ( have_posts() ) : ?>
+        <div class="faq-accordion">
+            <?php while ( have_posts() ) : the_post(); ?>
+            <article class="card">
+                <div class="card-header" id="heading-<?php the_ID(); ?>">
+                    <h2>
+                        <button class="btn btn-link" data-toggle="collapse" data-target="#post-<?php the_ID(); ?>" aria-expanded="true" aria-controls="heading-<?php the_ID(); ?>">
+                            <?php the_title(); ?>
+                        </button>
+                    </h2>
+                </div>
+                <div id="post-<?php the_ID(); ?>" class="collapse show" aria-labelledby="#heading-<?php the_ID(); ?>" data-parent="#faq-accordion">
+                    <div class="card-body">
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </article>
+            <?php endwhile; ?>
+        </div>
+        <?php endif; ?>
         <?php get_template_part( 'nav', 'below' ); ?>
     </section>
 <?php get_footer(); ?>
--- snap/snap-wp/entry-faq.php
+++ snap/snap-wp/entry-faq.php
@@ -1,6 +1,16 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('my-3 mb-4'); ?>>
     <header>
-        <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><span><?php the_title(); ?></span></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+        <?php
+            if ( is_singular() ) { echo '<h1 class="entry-title">'; }
+            else { echo '<h2 class="entry-title">'; }
+        ?>
+            <a class="accordion-link" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
+                <span><?php the_title(); ?></span>
+            </a>
+        <?php
+            if ( is_singular() ) { echo '</h1>'; }
+            else { echo '</h2>'; }
+        ?>
     </header>
     <?php get_template_part( 'entry', 'content' ); ?>
     <?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
