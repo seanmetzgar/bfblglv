f664014b574ac5fc0334e8312ffa31012652dd32
Sean Metzgar <sean.metzgar@gmail.com>
2018-04-19 16:02:37 -0400

    centering text

--- snap/snap-wp/archive-resources.php
+++ snap/snap-wp/archive-resources.php
@@ -6,7 +6,7 @@
 
         <div class="row justify-content-center mb-4">
             <div class="col-lg-5 col-md-6">
-                <h4>Community Partners Resources</h4>
+                <h4 class="text-center">Community Partners Resources</h4>
                 <div class="list-group">
                 <?php
                     // WP_Query arguments
