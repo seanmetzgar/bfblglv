2e83a3d748fae9beb9e30b12f19fd5397c746910
Sean Metzgar <sean.metzgar@gmail.com>
2018-04-23 21:44:30 -0400

    adding link to home page in footer (logo)

--- snap/snap-wp/footer.php
+++ snap/snap-wp/footer.php
@@ -5,7 +5,7 @@
         <div class="container justify-content-center">
             <div class="row">
                 <div class="col-md-2 col-12 footer-logo justify-content-center">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png">
+                    <a href="/"><img src="<?php echo get_template_directory_uri(); ?>/img/logos/bucks_logo_footer.png"></a>
                 </div>
                 <nav class="col-md-6 col-12 footer-nav text-center text-md-left">
                     <h3>Navigate</h3>
