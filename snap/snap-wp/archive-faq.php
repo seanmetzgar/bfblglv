f39029dc65801ffe01f2cf90c96a54ec086bc9a2
Sean Metzgar <sean.metzgar@gmail.com>
2018-05-01 01:54:25 -0400

    asdf

--- snap/snap-wp/archive-faq.php
+++ snap/snap-wp/archive-faq.php
@@ -6,24 +6,27 @@
     </section>
     <section class="container-fluid px-0">
         <?php if ( have_posts() ) : ?>
-        <div class="faq-accordion">
-            <?php while ( have_posts() ) : the_post(); ?>
-            <article class="card">
-                <div class="card-header" id="heading-<?php the_ID(); ?>">
-                    <a href="#post-<?php the_ID(); ?>" class="collapsed accordion-link" data-toggle="collapse" data-target="#post-<?php the_ID(); ?>" aria-expanded="true" aria-controls="heading-<?php the_ID(); ?>">
-                        <h5 class="entry-title">
-                            <?php the_title(); ?>
-                        </h5>
-                    </a>
-                </div>
-                <div id="post-<?php the_ID(); ?>" class="collapse" aria-labelledby="#heading-<?php the_ID(); ?>" data-parent="#faq-accordion">
-                    <div class="card-body">
-                        <?php the_content(); ?>
-                    </div>
-                </div>
-            </article>
-            <?php endwhile; ?>
+        <div class="row justify-content-center">
+            <div class="faq-accordion col-md-10 py-0">
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article class="card">
+                        <div class="card-header" id="heading-<?php the_ID(); ?>">
+                            <a href="#post-<?php the_ID(); ?>" class="collapsed accordion-link" data-toggle="collapse" data-target="#post-<?php the_ID(); ?>" aria-expanded="true" aria-controls="heading-<?php the_ID(); ?>">
+                                <h5 class="entry-title">
+                                    <?php the_title(); ?>
+                                </h5>
+                            </a>
+                        </div>
+                        <div id="post-<?php the_ID(); ?>" class="collapse" aria-labelledby="#heading-<?php the_ID(); ?>" data-parent="#faq-accordion">
+                            <div class="card-body">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                    </article>
+                <?php endwhile; ?>
+            </div>
         </div>
+
         <?php endif; ?>
     </section>
 <?php get_footer(); ?>
