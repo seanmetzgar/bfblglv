9925123d4e4b3c716e6e9d6ec0589db0de3dc52e
Sean Metzgar <sean.metzgar@gmail.com>
2018-05-01 00:23:04 -0400

    adding list

--- snap/snap-wp/page-locations.php
+++ snap/snap-wp/page-locations.php
@@ -17,27 +17,9 @@ get_header(); ?>
                 </header>
                 <section class="entry-content">
                     <?php the_content(); ?>
-                    <?php if( have_rows('flowchart_steps') ): ?>
-                        <div class="row justify-content-center">
-                            <ul class="flowchart col-lg-7 col-md-8 col-sm-10 col-12">
-                                <?php while ( have_rows('flowchart_steps') ) :
-                                    the_row(); $flowchart_image = get_sub_field('icon'); ?>
-                                    <li class="media my-3">
-                                        <?php if ($flowchart_image): ?>
-                                            <img class="align-self-center mr-3" src="<?php echo $flowchart_image; ?>">
-                                        <?php else: ?>
-                                            <img class="align-self-center mr-3" src="http://via.placeholder.com/100x100">
-                                        <?php endif; ?>
-                                        <div class="media-body">
-                                            <?php the_sub_field('description'); ?>
-                                        </div>
-                                    </li>
-                                <?php endwhile; ?>
-                            </ul>
-                        </div>
-
-                    <?php endif; ?>
-                    <div class="entry-links"><?php wp_link_pages(); ?></div>
+                    <section class="finder-search-results page-block">
+                        <ul class="results-list"></ul><!-- end ul.results-list -->
+                    </section>
                 </section>
             </article>
             <?php if ( ! post_password_required() ) comments_template( '', true ); ?>
