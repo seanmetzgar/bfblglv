d11894ffba274cbe924ee998ced9ae8b962c2867
Sean Metzgar <sean.metzgar@gmail.com>
2019-04-11 20:16:10 -0400

    Update HTML for newsletter form

--- bfblglv-wp/footer.php
+++ bfblglv-wp/footer.php
@@ -239,20 +239,52 @@
         	</div><!-- end div#loginLB -->
          	<div id="newsletterLB">
         		<h3>Sign up for<br />our newsletter</h3>
-        		<form name="ccoptin" id="form-ccsignup" action="http://visitor.constantcontact.com/d.jsp" method="post" target="_blank">
-					<fieldset class="input">
-						<input type="hidden" name="m" value="1102172366972"><input type="hidden" name="p" value="oi" />
-						<div class="nlEmail">
-							<label for="ea">Enter your email address</label>
-							<input type="text" name="ea" id="ea" size="25" class="text" placeholder="Enter your email address" value="" />
-						</div><!-- end div.nlEmail -->
-						<div class="nlSubmit">
-							<input type="submit" name="Sign Up" class="bfblButtonLink" value="Sign Up" />
-						</div><!-- end div.nlSubmit -->
-					</fieldset>
-				</form>
+						<!-- START: Mailchimp Signup Form -->
+						<div id="mc_embed_signup">
+							<form action="https://buylocalglv.us19.list-manage.com/subscribe/post?u=386c91afa5f36c7d7fc05dfd7&amp;id=f1f6d8056f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+								<div id="mc_embed_signup_scroll">
 
+									<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+									<div class="mc-field-group">
+										<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+										</label>
+										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+									</div>
+									<div class="mc-field-group">
+										<label for="mce-FNAME">First Name </label>
+										<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+									</div>
+									<div class="mc-field-group">
+										<label for="mce-LNAME">Last Name </label>
+										<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>
+									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_386c91afa5f36c7d7fc05dfd7_f1f6d8056f" tabindex="-1" value=""></div>
+									<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+								</div>
+							</form>
+						</div>
+						<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+						<script type='text/javascript'>
+							(function($) {
+								window.fnames = new Array();
+								window.ftypes = new Array();
 
+								fnames[0]='EMAIL';
+								ftypes[0]='email';
+
+								fnames[1]='FNAME';
+								ftypes[1]='text';
+
+								fnames[2]='LNAME';
+								ftypes[2]='text';
+							}(jQuery));
+							var $mcj = jQuery.noConflict(true);
+						</script>
+						<!-- END: Mailchimp Signup Form -->
         	</div><!-- end div#newsletterLB -->
         </div><!-- end div.bfblLightboxes -->
         <?php wp_footer(); ?>
